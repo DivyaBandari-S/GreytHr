11fa8f86adbbaa38d1cdedd87a8868f3a5992d4f
Nikhitha <nadimintinikhitha1998@gmail.com>
2024-08-05 11:45:30 +0530

    new changes

---
+++ storage/framework/views/c264f479a7621e32dda91bbc75941b0a.php
@@ -0,0 +1,72 @@
+<div>
+    <div class="container">
+        <div class="row m-0 p-0">
+            <div class="col-md-12 text-right d-flex justify-content-end">
+                <select id="yearSelect" wire:change="selectYear($event.target.value)" class="dropdownPlaceholder">
+                    <option class="optionDropdown" value="<?php echo e($previousYear); ?>" <?php echo e($selectedYear == $previousYear ? 'selected' : ''); ?>><?php echo e($previousYear); ?></option>
+                    <option class="optionDropdown" value="<?php echo e($initialSelectedYear); ?>" <?php echo e($selectedYear == $initialSelectedYear ? 'selected' : ''); ?>><?php echo e($initialSelectedYear); ?></option>
+                    <option class="optionDropdown" value="<?php echo e($nextYear); ?>" <?php echo e($selectedYear == $nextYear ? 'selected' : ''); ?>><?php echo e($nextYear); ?></option>
+                </select>
+            </div>
+        </div>
+    </div>
+
+    <div class="hol-container" id="calendar<?php echo e($selectedYear); ?>">
+        <div class="row m-0">
+            <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $calendarData->sortBy('date')->groupBy(function($entry) {
+            return date('F Y', strtotime($entry->date));
+            }); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $month => $entries): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <div class="col-md-3">
+                <div class="inner-container">
+                    <div class="headerMonth">
+                        <h6><?php echo e($month); ?></h6>
+                    </div>
+                    <!--[if BLOCK]><![endif]--><?php if($entries->isEmpty() || $entries->every(function ($entry) {
+                    return empty($entry->festivals);
+                    })): ?>
+                    <div class="no-holidays">
+                        <h6>No holidays</h6>
+                    </div>
+                    <?php else: ?>
+                    <div class="group py-3 px-3">
+                        <!--[if BLOCK]><![endif]--><?php $__currentLoopData = $entries; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $entry): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <div class="fest d-flex flex-row align-items-center" style="gap:10px;">
+                            <h5 class="p-0 m-0"><?php echo e(date('d', strtotime($entry->date))); ?><span>
+                                    <p class="mb-0" style="font-size: 10px;"><?php echo e(substr($entry->day, 0, 3)); ?></p>
+                                </span></h5>
+                            <p class="mb-0 festivals" style="font-size: 12px;"><?php echo e($entry->festivals); ?></p>
+                        </div>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><!--[if ENDBLOCK]><![endif]-->
+                    </div>
+                    <?php endif; ?><!--[if ENDBLOCK]><![endif]-->
+                </div>
+            </div>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?><!--[if ENDBLOCK]><![endif]-->
+        </div>
+    </div>
+    <div style="text-align: center; margin: 30px auto;">
+        <!--[if BLOCK]><![endif]--><?php if($selectedYear == $nextYear && $calendarData->where('year', $nextYear)->isEmpty()): ?>
+        <div class="bg-white rounded border p-3" style="margin: 50px auto; width:80%;">
+            <p style="font-size: 16px; color: #721c24; font-weight: bold;">It’s lonely here!</p>
+            <p style="font-size: 12px; color:#778899;">Your HR department is yet to publish the holiday list for the year <?php echo e($nextYear); ?>, check again later.</p>
+        </div>
+        <?php endif; ?><!--[if ENDBLOCK]><![endif]-->
+    </div>
+
+
+    <script>
+        $(document).ready(function() {
+            // Initially, show the calendar for the selected year
+            var selectedYear = $("#yearSelect").val();
+            $("#calendar" + selectedYear).show();
+
+            $("#yearSelect").change(function() {
+                var selectedYear = $(this).val();
+                // Hide all calendars
+                $(".hol-container").hide();
+                // Show the calendar based on the selected year
+                $("#calendar" + selectedYear).show();
+            });
+        });
+    </script>
+</div><?php /**PATH C:\xampp\htdocs\GreytHr\resources\views/livewire/holiday-calender.blade.php ENDPATH**/ ?>
