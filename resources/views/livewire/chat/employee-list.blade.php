79e68ba8a9022e49f4d3290a8461d5d73eb21d05
archanagoud28 <125112613+archanagoud28@users.noreply.github.com>
2024-08-13 13:06:38 +0530

    few changes

--- resources/views/livewire/chat/employee-list.blade.php
+++ resources/views/livewire/chat/employee-list.blade.php
@@ -7,63 +7,63 @@
                     <h6 class="text-start text-5xl font-bold py-3 px-4">Departments</h6>
                     <select class="form-control mb-4" wire:model="selectedDepartment" wire:change="filter">
                         <option value="">All Departments</option>
-
+ 
                     </select>
                 </div>
             </div>
         </div>
-
+ 
         <!-- Right Side: Search and Employee Details -->
         <div class="col-md-9">
             <div class="d-flex justify-content-between align-items-center mb-2">
                 <div class="col-md-6">
                     <h6 class="text-start text-5xl font-bold py-3 px-4 employees-details-chat">Users</h6>
                 </div>
-                <div class="col-md-6 input-group">
-                    <input type="text" class="form-control3" placeholder="Search..."
-                        wire:model="searchTerm" aria-label="Search" aria-describedby="search-addon" style="width:220px;height:40px;border-radius:5px; border: 1px solid #ced4da;padding: 0 10px;"
-                        wire:input="filter">
-                    <button class="submit-btn" wire:click="filter" id="search-addon"
-                        style="height:40px; line-height: 2;">Search</button>
+                <div class="col-md-6 ">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search..."
+                            wire:model="searchTerm" aria-label="Search" aria-describedby="search-addon"
+                            wire:input="filter">
+                        <button class="submit-btn" wire:click="filter" id="search-addon">Search</button>
+                    </div>
                 </div>
             </div>
-
+ 
             <div class="row m-0 p-0 justify-content-center"
                 style="overflow-y: auto; height: 100vh;">
                 @forelse ($employeeDetails as $key => $employee)
-                    <div class="col-lg-3 mb-6"> <!-- Increase width to col-lg-4 -->
-                        <div class="card" style="width: 200px;">
-                            <div class="col d-flex align-items-center justify-content-center mt-4">
-                        
-                            @if ($employee->image_url)
-    <img src="{{ $employee->image_url }}" height="50" width="50" style="border-radius:50%;" alt="Employee Image">
-@elseif ($employee->gender == 'Male')
-    <img src="https://www.kindpng.com/picc/m/252-2524695_dummy-profile-image-jpg-hd-png-download.png" height="50" width="50" style="border-radius:50%;" alt="Default Male Profile">
-@elseif ($employee->gender == 'Female')
-    <img src="https://th.bing.com/th/id/OIP.16PsNaosyhVxpn3hmvC46AHaHa?w=199&h=199&c=7&r=0&o=5&dpr=1.5&pid=1.7" height="50" width="50" style="border-radius:50%;" alt="Default Female Profile">
-@else
-    <img src="https://th.bing.com/th/id/OIP.Ii15573m21uyos5SZQTdrAHaHa?rs=1&pid=ImgDetMain" height="50" width="50" style="border-radius:50%;" alt="Default Profile Image">
-@endif
-
-
+                <div class="col-md-4 mb-3"> <!-- Increase width to col-lg-4 -->
+                    <div class="card">
+                        <div class="col d-flex align-items-center justify-content-center mt-4">
+                            @if(!empty($employee->image) && $employee->image !== 'null')
+                            <div>
+                                <img src="{{ 'data:image/jpeg;base64,' . base64_encode($employee->image)}}" height="50"
+                                    width="50" style="border-radius:50%;">
                             </div>
-                            <div class="card-body text-center">
-                                <div class="chat-employee-name">
-                                    {{ ucwords(strtolower($employee->first_name)) }}&nbsp;{{ ucwords(strtolower($employee->last_name)) }}
-                                </div>
-                                @php
-                                    $jobTitle = $employee->job_title;
-                                    $convertedTitle = preg_replace('/\bII\b/', 'I', $jobTitle);
-                                    $convertedTitle = preg_replace('/\bII\b/', 'II', $jobTitle);
-                                    $convertedTitle = preg_replace('/\bIII\b/', 'III', $convertedTitle);
-                                @endphp
-                                <p class="card-text px-4 mb-0" style="display: inline-block;">{{ $convertedTitle }}</p>
-                                <div class="d-flex justify-content-between mt-3">
-                                    <div class="chat-emp-head d-flex flex-column align-items-start gap-1">
-                                        <span>Employee Id</span>
-                                        <span>Department</span>
-                                        <span>Join Date</span>
-
+                            @else
+                            <div>
+                                <img src="{{ asset('images/user.jpg') }}"
+                                    height="50" width="50" alt="Default Image">
+                            </div>
+                            @endif
+                        </div>
+                        <div class="card-body text-center">
+                            <div class="d-flex align-items-center justify-content-center">
+                                <span class="chat-employee-name" title="{{ ucwords(strtolower($employee->first_name)) }}&nbsp;{{ ucwords(strtolower($employee->last_name)) }}">{{ ucwords(strtolower($employee->first_name)) }}&nbsp;{{ ucwords(strtolower($employee->last_name)) }}</span>
+                            </div>
+                            @php
+                            $jobTitle = $employee->job_title;
+                            $convertedTitle = preg_replace('/\bII\b/', 'I', $jobTitle);
+                            $convertedTitle = preg_replace('/\bII\b/', 'II', $jobTitle);
+                            $convertedTitle = preg_replace('/\bIII\b/', 'III', $convertedTitle);
+                            @endphp
+                            <p class="card-text px-4 mb-0" style="display: inline-block;">{{ $convertedTitle }}</p>
+                            <div class="d-flex justify-content-between mt-3">
+                                <div class="chat-emp-head d-flex flex-column align-items-start gap-1">
+                                    <span>Employee Id</span>
+                                    <span>Department</span>
+                                    <span>Join Date</span>
+ 
                                 </div>
                                 <div class="chat-emp-details d-flex flex-column align-items-end gap-1">
                                     <span>{{ $employee->emp_id }}</span>
@@ -102,3 +102,4 @@
         </div>
     </div>
 </div>
+ 
