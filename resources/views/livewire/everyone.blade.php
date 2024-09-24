6a2c344db666b5281c538f8471b1a2abece4c0e7
archanagoud28 <125112613+archanagoud28@users.noreply.github.com>
2024-09-24 10:38:15 +0530

    changes

--- resources/views/livewire/everyone.blade.php
+++ resources/views/livewire/everyone.blade.php
@@ -159,7 +159,7 @@
     <!-- Additional row -->
     <div class="row mt-2 d-flex">
 
-        <div class="col feeds-custom-menu bg-white p-3">
+        <div class="col-md-3 feeds-custom-menu bg-white p-3">
 
             <p class="feeds-left-menu">Filters</p>
             <hr style="width: 100%;border-bottom: 1px solid grey;">
