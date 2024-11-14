1efcd12e591df88aba0f301b5115c393c425f1c0
DivyaBandari-S <bandaridivya1@gmail.com>
2024-11-14 12:25:13 +0530

    main changes

--- database/migrations/2023_10_27_1234576786_create_help_desks_table.php
+++ database/migrations/2023_10_27_1234576786_create_help_desks_table.php
@@ -12,21 +12,24 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('help_desks', function (Blueprint $table) {
-            $table->smallInteger('id')->autoIncrement();
-            $table->string('emp_id',10);
-            $table->string('category',50);
-            $table->string('mail',50);
-            $table->string('distributor_name',100);
-            $table->string('mobile',20);
-            $table->text('subject');
+            $table->id();
+            $table->string('emp_id');
+            $table->string('category');
+            $table->string('mail');
+            $table->string('distributor_name');
+            $table->string('mobile');
+            $table->string('subject');
             $table->text('description');
             $table->text('active_comment')->nullable();
              $table->text('inprogress_remarks')->nullable();
+             $table->text('rejection_reason')->nullable();
              $table->string('assign_to')->nullable();
             $table->string('file_path')->nullable(); // Path to attached file (nullable)
+            $table->string('file_name')->nullable();
+            $table->string('mime_type')->nullable();
             $table->string('cc_to')->nullable(); // CC to field (nullable)
-            $table->string('status',20)->default('Recent'); // CC to field (nullable)
-            $table->enum('selected_equipment',['keyboard', 'mouse', 'monitor','headset']);
+            $table->string('status')->default('Recent'); // CC to field (nullable)
+            $table->enum('selected_equipment',['keyboard', 'mouse', 'monitor','headset','others']);
             $table->enum('priority', ['High', 'Medium', 'Low']); // Priority field with enum values
             $table->timestamps();
 
