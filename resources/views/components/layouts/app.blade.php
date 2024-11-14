65291a399c5419a6d6bdd84c4942b39b2c800f7a
DivyaBandari-S <bandaridivya1@gmail.com>
2024-11-14 10:34:18 +0530

    new changes

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -8,7 +8,7 @@
     @guest
       <link rel="icon" type="image/x-icon" href="{{ asset('/images/fav.jpeg') }}">
     <title>
-        HR Xpert
+        hr xpert
     </title>
     @endguest
     @auth('emp')
@@ -62,7 +62,7 @@ if ($companyIds) {
         @endphp
       <link rel="icon" type="image/x-icon" href="{{ asset('/images/fav.jpeg') }}">
         <title>
-        HR Xpert
+        hr xpert
         </title>
     @endauth
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
