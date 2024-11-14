652ea74d5bda47a4101d7aef85ebe476f7813257
archanagoud28 <125112613+archanagoud28@users.noreply.github.com>
2024-11-14 16:59:02 +0530

    changes

--- app/Livewire/Catalog.php
+++ app/Livewire/Catalog.php
@@ -184,7 +184,7 @@ class Catalog extends Component
         $this->selectedPeople=[];
         $this->addselectedPeople=[];
         $this->isNames=false;
-        $this->filer();
+    $this->searchTerm = '';
 
     }
  
