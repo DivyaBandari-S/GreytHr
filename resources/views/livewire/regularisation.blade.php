96d30ef73cd7f1650ae1047db3522a8c80c321e1
prpriyadarshi <priyadarshipranita72@gmail.com>
2024-11-25 15:56:17 +0530

    local changes

--- resources/views/livewire/regularisation.blade.php
+++ resources/views/livewire/regularisation.blade.php
@@ -407,12 +407,12 @@
         .hidden-pending-box {
 
             background-color: #fff;
-            margin-top: 230px;
+            margin-top: 200px;
             padding: 20px;
             border-radius: 5px;
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
             text-align: center;
-            margin-left: 550px;
+            margin-left: 500px;
             position: absolute;
             height: 320px;
             width: 900px;
