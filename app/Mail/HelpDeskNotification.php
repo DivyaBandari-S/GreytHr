f25d1df1eb4433f91b285233449b22efe0065481
archanagoud28 <125112613+archanagoud28@users.noreply.github.com>
2024-11-26 16:27:03 +0530

    changes

--- app/Mail/HelpDeskNotification.php
+++ app/Mail/HelpDeskNotification.php
@@ -10,6 +10,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
 
 class HelpDeskNotification extends Mailable
 {
@@ -35,26 +36,30 @@ class HelpDeskNotification extends Mailable
      */
     public function __construct(HelpDesks $helpDesk, $firstName, $lastName)
     {
-        
         $this->helpDesk = $helpDesk;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
-        $this->RequestId = $this->helpDesk->request_id ;
-        $this->requestCreatedBy = EmployeeDetails::where('emp_id', '=', $this->helpDesk->emp_id)->first();
+        $this->RequestId = $helpDesk->request_id; // Fetch directly from the passed model
+    
+        $this->requestCreatedBy = EmployeeDetails::where('emp_id', $helpDesk->emp_id)->first();
+    
         if ($this->requestCreatedBy) {
-            // Access the first_name, last_name, and emp_id directly
             $this->createdbyFirstName = $this->requestCreatedBy->first_name;
             $this->createdbyLastName = $this->requestCreatedBy->last_name;
             $this->employeeId = $this->requestCreatedBy->emp_id;
-
         } else {
-            // Handle not found scenario
             $this->createdbyFirstName = 'N/A';
             $this->createdbyLastName = 'N/A';
             $this->employeeId = 'N/A';
         }
+    
+        Log::info('Notification Constructor Data', [
+            'Request ID' => $this->RequestId,
+            'Created By' => "{$this->createdbyFirstName} {$this->createdbyLastName}",
+            'Employee ID' => $this->employeeId,
+        ]);
     }
-
+    
     /**
      * Build the message.
      *
@@ -62,9 +67,20 @@ class HelpDeskNotification extends Mailable
      */
     public function build()
     {
-        return $this->subject('New Request Created' . $this->helpDesk->request_id . ' created by ' . $this->createdbyFirstName . ' ' . $this->createdbyLastName)
-                    ->view('emails.helpdesk-notification');
+        // Ensure $this->RequestId is used instead of $RequestId (which is not defined in the method scope)
+        return $this->subject('Request ' . $this->RequestId . ' created by ' . $this->createdbyFirstName . ' ' . $this->createdbyLastName)
+                    ->view('emails.helpdesk-notification')
+                    ->with([
+                        'RequestId' => $this->RequestId,
+                        'createdbyFirstName' => $this->createdbyFirstName,
+                        'createdbyLastName' => $this->createdbyLastName,
+                        'employeeId' => $this->employeeId,
+                        'firstName' => $this->firstName,
+                        'lastName' => $this->lastName,
+                        'helpDesk' => $this->helpDesk,
+                    ]);
     }
+    
     public function content(): Content
     {
         return new Content(
