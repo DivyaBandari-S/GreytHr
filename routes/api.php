093a23e6db75bab0e61f8f3e7132eec242fe1fad
archanagoud28 <125112613+archanagoud28@users.noreply.github.com>
2025-03-20 12:34:26 +0530

    mychanges

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\EmployeeDetailsController;
 use App\Http\Controllers\API\EmpPersonalInfoController;
+use App\Http\Controllers\API\FeedsController;
 use App\Http\Controllers\API\HolidayListController;
 use App\Http\Controllers\API\ShowSalaryController;
 use App\Http\Controllers\API\SwipeController;
@@ -31,6 +32,7 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::post('me', [AuthController::class, 'empDetails']);
     Route::get('empdetails', [AuthController::class, 'getEmployeeDetails']); // Fetch Employee Details via Token
+    Route::post('allempdetails', [AuthController::class, 'getAllEmployeeDetails']); 
     Route::post('swipe', [SwipeController::class, 'swipe']); // Handle Swipe (Single API for both IN & OUT)
     Route::post('holidays', [HolidayListController::class, 'index']);
     Route::post('/holidays/upcoming', [HolidayListController::class, 'getUpcomingHolidays']);
@@ -39,5 +41,7 @@ Route::middleware(['auth:api'])->group(function () {
     Route::post('/employee/personal/view', [EmpPersonalInfoController::class, 'show']);
     Route::post('/employee/personal/update', [EmpPersonalInfoController::class, 'update']);
     Route::post('showsal', [ShowSalaryController::class, 'showSalary']);
+    Route::post('getfeed', [FeedsController::class, 'getEmployeeFeed']);
+
 
 });
