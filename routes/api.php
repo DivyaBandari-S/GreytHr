5e853eedd38e2ee3f0d578a287569beda9bbeab9
sssreddys <saragada566@gmail.com>
2025-03-21 12:35:27 +0530

    ip issues and password rest notifications  issues resolved

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\API\EmpPersonalInfoController;
 use App\Http\Controllers\API\HolidayListController;
 use App\Http\Controllers\API\ShowSalaryController;
 use App\Http\Controllers\API\SwipeController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -15,17 +16,15 @@ Route::get('/user', function (Request $request) {
 Route::post('login', [AuthController::class, 'login']); // Login with email or emp_id
 
 Route::middleware(['auth:api'])->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'empDetails']);
-    Route::get('empdetails', [AuthController::class, 'getEmployeeDetails']); // Fetch Employee Details via Token
-    Route::post('swipe', [SwipeController::class, 'swipe']); // Handle Swipe (Single API for both IN & OUT)
-    Route::post('holidays', [HolidayListController::class, 'index']);
-    Route::post('/holidays/upcoming', [HolidayListController::class, 'getUpcomingHolidays']);
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('emp-details', [AuthController::class, 'getEmployeeDetails'])->name('emp-details'); // Fetch Employee Details via Token
+    Route::post('swipe', [SwipeController::class, 'swipe'])->name('swipe'); // Handle Swipe (Single API for both IN & OUT)
+    Route::post('holidays', [HolidayListController::class, 'index'])->name('holidays'); // Handle
+    Route::post('holidays/upcoming', [HolidayListController::class, 'getUpcomingHolidays'])->name('holidays-upcoming'); // Handle
     // Route::post('/employee/update', [EmployeeDetailsController::class, 'update']);
-    Route::post('/employee/show', [EmployeeDetailsController::class, 'show']);
-    Route::post('/employee/personal/view', [EmpPersonalInfoController::class, 'show']);
-    Route::post('/employee/personal/update', [EmpPersonalInfoController::class, 'update']);
-    Route::post('showsal', [ShowSalaryController::class, 'showSalary']);
-
+    Route::post('employee/show', [EmployeeDetailsController::class, 'show'])->name('employee-show');
+    Route::post('employee/personal/view', [EmpPersonalInfoController::class, 'show'])->name('employee-personal-view');
+    Route::post('employee/personal/update', [EmpPersonalInfoController::class, 'update'])->name('employee-personal-update');
+    Route::post('show-sal', [ShowSalaryController::class, 'showSalary'])->name('show-sal');
 });
